unik-name/explorer

commit 4a22829f3c05db4fd18d35a24543e0ad55ce2871

feat: setup unikname theme

src/components/header/HeaderSearch.vue
@@ -25,7 +25,7 @@
 
     <button
       :disabled="!hasInput"
-      class="search-icon text-grey hover:text-blue p-3 md:p-4 transition"
+      class="search-icon text-grey hover:text-orange p-3 md:p-4 transition"
       @click="search"
     >
       <svg
@@ -34,134 +34,156 @@
         height="20"
         viewBox="0 0 1792 1792"
         xmlns="http://www.w3.org/2000/svg"
-      ><path d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z" /></svg>
+      >
+        <path d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z" /></svg>
     </button>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import SearchService from '@/services/search'
-import { mapGetters } from 'vuex'
+import SearchService from "@/services/search";
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'HeaderSearch',
+  name: "HeaderSearch",
 
   data: () => ({
     query: null,
     nothingFound: false,
     searchCount: 0,
-    placeholder: 'Search'
+    placeholder: "Search"
   }),
 
   computed: {
-    ...mapGetters('delegates', ['delegates']),
-    ...mapGetters('ui', ['nightMode']),
-    ...mapGetters('network', ['knownWallets']),
+    ...mapGetters("delegates", ["delegates"]),
+    ...mapGetters("ui", ["nightMode"]),
+    ...mapGetters("network", ["knownWallets"]),
 
-    hasInput () {
-      return !!this.query
+    hasInput() {
+      return !!this.query;
     }
   },
 
-  mounted () {
-    this.$refs.search.focus()
+  mounted() {
+    this.$refs.search.focus();
 
-    const WIDTH_THRESHOLD = 1024
-    const widthQuery = window.matchMedia(`(max-width: ${WIDTH_THRESHOLD}px)`)
+    const WIDTH_THRESHOLD = 1024;
+    const widthQuery = window.matchMedia(`(max-width: ${WIDTH_THRESHOLD}px)`);
 
-    widthQuery.addListener(e => this.setMobilePlaceholder(e.matches))
+    widthQuery.addListener(e => this.setMobilePlaceholder(e.matches));
 
-    this.setMobilePlaceholder(window.innerWidth < WIDTH_THRESHOLD)
+    this.setMobilePlaceholder(window.innerWidth < WIDTH_THRESHOLD);
   },
 
   methods: {
-    async search () {
+    async search() {
       if (!this.query) {
-        return
+        return;
       }
 
-      this.nothingFound = false
-      this.searchCount = 0
-      this.query = this.query.trim()
+      this.nothingFound = false;
+      this.searchCount = 0;
+      this.query = this.query.trim();
 
-      const address = this.findByNameInKnownWallets(this.query)
+      const address = this.findByNameInKnownWallets(this.query);
       if (address) {
-        this.changePage('wallet', { address: address })
-        return
+        this.changePage("wallet", { address: address });
+        return;
       } else {
-        this.updateSearchCount({ message: 'No known wallet with that name could be found' })
+        this.updateSearchCount({
+          message: "No known wallet with that name could be found"
+        });
       }
 
-      const del = this.delegates.find(d => d.username === this.query.toLowerCase())
+      const del = this.delegates.find(
+        d => d.username === this.query.toLowerCase()
+      );
       if (del) {
-        this.changePage('wallet', { address: del.address })
-        return
+        this.changePage("wallet", { address: del.address });
+        return;
       } else {
-        this.updateSearchCount({ message: 'No delegate with that username could be found' })
+        this.updateSearchCount({
+          message: "No delegate with that username could be found"
+        });
       }
 
       try {
-        const responseAddress = await SearchService.walletByAddress(this.query)
-        this.changePage('wallet', { address: responseAddress.address })
-        return
-      } catch (e) { this.updateSearchCount(e) }
+        const responseAddress = await SearchService.walletByAddress(this.query);
+        this.changePage("wallet", { address: responseAddress.address });
+        return;
+      } catch (e) {
+        this.updateSearchCount(e);
+      }
 
       try {
-        const responseDelegate = await SearchService.delegateByQuery(this.query.toLowerCase())
-        this.changePage('wallet', { address: responseDelegate.address })
-        return
-      } catch (e) { this.updateSearchCount(e) }
+        const responseDelegate = await SearchService.delegateByQuery(
+          this.query.toLowerCase()
+        );
+        this.changePage("wallet", { address: responseDelegate.address });
+        return;
+      } catch (e) {
+        this.updateSearchCount(e);
+      }
 
       try {
-        const responseBlock = await SearchService.blockByQuery(this.query)
-        this.changePage('block', { id: responseBlock.id })
-        return
-      } catch (e) { this.updateSearchCount(e) }
+        const responseBlock = await SearchService.blockByQuery(this.query);
+        this.changePage("block", { id: responseBlock.id });
+        return;
+      } catch (e) {
+        this.updateSearchCount(e);
+      }
 
       try {
-        const responseTransaction = await SearchService.transactionById(this.query)
-        this.changePage('transaction', { id: responseTransaction.id })
-        return
-      } catch (e) { this.updateSearchCount(e) }
+        const responseTransaction = await SearchService.transactionById(
+          this.query
+        );
+        this.changePage("transaction", { id: responseTransaction.id });
+        return;
+      } catch (e) {
+        this.updateSearchCount(e);
+      }
     },
 
-    updateSearchCount (err) {
+    updateSearchCount(err) {
       if (err !== null) {
-        console.log(err.message || err.data.error)
+        console.log(err.message || err.data.error);
       }
 
       // Increment counter to keep track of whether we found anything
-      this.searchCount += 1
-      if (this.searchCount === 6) { // Should match total amount of callbacks
-        this.nothingFound = true
-        setTimeout(() => (this.nothingFound = false), 1500)
+      this.searchCount += 1;
+      if (this.searchCount === 6) {
+        // Should match total amount of callbacks
+        this.nothingFound = true;
+        setTimeout(() => (this.nothingFound = false), 1500);
       }
     },
 
-    setMobilePlaceholder (showMobile) {
+    setMobilePlaceholder(showMobile) {
       this.placeholder = showMobile
-        ? this.$i18n.t('Search')
-        : this.$i18n.t('Find a block, transaction, address or delegate')
+        ? this.$i18n.t("Search")
+        : this.$i18n.t("Find a block, transaction, address or delegate");
     },
 
-    changePage (name, params) {
-      this.$router.push({ name, params })
-      this.$store.dispatch('ui/setHeaderType', null)
+    changePage(name, params) {
+      this.$router.push({ name, params });
+      this.$store.dispatch("ui/setHeaderType", null);
     },
 
-    findByNameInKnownWallets (name) {
+    findByNameInKnownWallets(name) {
       if (name !== null) {
         for (const address in this.knownWallets) {
           if (this.knownWallets.hasOwnProperty(address)) {
-            if (name.toLowerCase() === this.knownWallets[address].toLowerCase()) {
-              return address
+            if (
+              name.toLowerCase() === this.knownWallets[address].toLowerCase()
+            ) {
+              return address;
             }
           }
         }
       }
     }
   }
-}
+};
 </script>
 
 <style>

src/assets/images/currencies/AUD.svg
@@ -4,7 +4,7 @@
 	 viewBox="0 0 22.6 16.2" style="enable-background:new 0 0 22.6 16.2;" xml:space="preserve">
 <style type="text/css">
 	.st0{enable-background:new    ;}
-	.st1{fill:#037CFF;}
+	.st1{fill:#ffa407;}
 </style>
 <g id="Shape_1_копия_1_" class="st0">
 	<g id="Shape_1_копия">

src/assets/images/currencies/BRL.svg
@@ -4,7 +4,7 @@
 	 viewBox="0 0 21.4 16.4" style="enable-background:new 0 0 21.4 16.4;" xml:space="preserve">
 <style type="text/css">
 	.st0{enable-background:new    ;}
-	.st1{fill:#037CFF;}
+	.st1{fill:#ffa407;}
 </style>
 <g id="Shape_1_копия_1_" class="st0">
 	<g id="Shape_1_копия">

src/assets/images/currencies/BTC.svg
@@ -3,7 +3,7 @@
 <svg version="1.1" id="Forma_1_1_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 12.7 16.7" style="enable-background:new 0 0 12.7 16.7;" xml:space="preserve">
 <style type="text/css">
-	.st0{fill:#037CFF;}
+	.st0{fill:#ffa407;}
 </style>
 <g id="Forma_1">
 	<g>

src/assets/images/currencies/CAD.svg
@@ -4,7 +4,7 @@
 	 viewBox="0 0 21.6 16.2" style="enable-background:new 0 0 21.6 16.2;" xml:space="preserve">
 <style type="text/css">
 	.st0{enable-background:new    ;}
-	.st1{fill:#037CFF;}
+	.st1{fill:#ffa407;}
 </style>
 <g id="Shape_1_копия_1_" class="st0">
 	<g id="Shape_1_копия">

src/assets/images/currencies/CHF.svg
@@ -3,7 +3,7 @@
 <svg version="1.1" id="Forma_1_1_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 12.6 16.4" style="enable-background:new 0 0 12.6 16.4;" xml:space="preserve">
 <style type="text/css">
-	.st0{fill:#037CFF;}
+	.st0{fill:#ffa407;}
 </style>
 <g id="Forma_1">
 	<g>

src/assets/images/currencies/CNY.svg
@@ -3,7 +3,7 @@
 <svg version="1.1" id="Фигура_2_1_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
 	 y="0px" viewBox="0 0 372 401.3" style="enable-background:new 0 0 372 401.3;" xml:space="preserve">
 <style type="text/css">
-	.st0{fill:#037CFF;}
+	.st0{fill:#ffa407;}
 </style>
 <g id="Фигура_2">
 	<g>

src/assets/images/currencies/ETH.svg
@@ -3,7 +3,7 @@
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 141.2 226.8" style="enable-background:new 0 0 141.2 226.8;" xml:space="preserve">
 <style type="text/css">
-	.st0{fill:#037CFF;}
+	.st0{fill:#ffa407;}
 </style>
 <g>
 	<polygon class="st0" points="68.4,157 68.4,87 0,116.9 	"/>

commit e0e708692131fe82b1aab9cc4545cf65e12b6113

chores: fix lint errors introduced by 4a22829

src/components/header/HeaderSearch.vue
@@ -41,149 +41,149 @@
 </template>
 
 <script type="text/ecmascript-6">
-import SearchService from "@/services/search";
-import { mapGetters } from "vuex";
+import SearchService from '@/services/search'
+import { mapGetters } from 'vuex'
 
 export default {
-  name: "HeaderSearch",
+  name: 'HeaderSearch',
 
   data: () => ({
     query: null,
     nothingFound: false,
     searchCount: 0,
-    placeholder: "Search"
+    placeholder: 'Search'
   }),
 
   computed: {
-    ...mapGetters("delegates", ["delegates"]),
-    ...mapGetters("ui", ["nightMode"]),
-    ...mapGetters("network", ["knownWallets"]),
+    ...mapGetters('delegates', ['delegates']),
+    ...mapGetters('ui', ['nightMode']),
+    ...mapGetters('network', ['knownWallets']),
 
-    hasInput() {
-      return !!this.query;
+    hasInput () {
+      return !!this.query
     }
   },
 
-  mounted() {
-    this.$refs.search.focus();
+  mounted () {
+    this.$refs.search.focus()
 
-    const WIDTH_THRESHOLD = 1024;
-    const widthQuery = window.matchMedia(`(max-width: ${WIDTH_THRESHOLD}px)`);
+    const WIDTH_THRESHOLD = 1024
+    const widthQuery = window.matchMedia(`(max-width: ${WIDTH_THRESHOLD}px)`)
 
-    widthQuery.addListener(e => this.setMobilePlaceholder(e.matches));
+    widthQuery.addListener(e => this.setMobilePlaceholder(e.matches))
 
-    this.setMobilePlaceholder(window.innerWidth < WIDTH_THRESHOLD);
+    this.setMobilePlaceholder(window.innerWidth < WIDTH_THRESHOLD)
   },
 
   methods: {
-    async search() {
+    async search () {
       if (!this.query) {
-        return;
+        return
       }
 
-      this.nothingFound = false;
-      this.searchCount = 0;
-      this.query = this.query.trim();
+      this.nothingFound = false
+      this.searchCount = 0
+      this.query = this.query.trim()
 
-      const address = this.findByNameInKnownWallets(this.query);
+      const address = this.findByNameInKnownWallets(this.query)
       if (address) {
-        this.changePage("wallet", { address: address });
-        return;
+        this.changePage('wallet', { address: address })
+        return
       } else {
         this.updateSearchCount({
-          message: "No known wallet with that name could be found"
-        });
+          message: 'No known wallet with that name could be found'
+        })
       }
 
       const del = this.delegates.find(
         d => d.username === this.query.toLowerCase()
-      );
+      )
       if (del) {
-        this.changePage("wallet", { address: del.address });
-        return;
+        this.changePage('wallet', { address: del.address })
+        return
       } else {
         this.updateSearchCount({
-          message: "No delegate with that username could be found"
-        });
+          message: 'No delegate with that username could be found'
+        })
       }
 
       try {
-        const responseAddress = await SearchService.walletByAddress(this.query);
-        this.changePage("wallet", { address: responseAddress.address });
-        return;
+        const responseAddress = await SearchService.walletByAddress(this.query)
+        this.changePage('wallet', { address: responseAddress.address })
+        return
       } catch (e) {
-        this.updateSearchCount(e);
+        this.updateSearchCount(e)
       }
 
       try {
         const responseDelegate = await SearchService.delegateByQuery(
           this.query.toLowerCase()
-        );
-        this.changePage("wallet", { address: responseDelegate.address });
-        return;
+        )
+        this.changePage('wallet', { address: responseDelegate.address })
+        return
       } catch (e) {
-        this.updateSearchCount(e);
+        this.updateSearchCount(e)
       }
 
       try {
-        const responseBlock = await SearchService.blockByQuery(this.query);
-        this.changePage("block", { id: responseBlock.id });
-        return;
+        const responseBlock = await SearchService.blockByQuery(this.query)
+        this.changePage('block', { id: responseBlock.id })
+        return
       } catch (e) {
-        this.updateSearchCount(e);
+        this.updateSearchCount(e)
       }
 
       try {
         const responseTransaction = await SearchService.transactionById(
           this.query
-        );
-        this.changePage("transaction", { id: responseTransaction.id });
-        return;
+        )
+        this.changePage('transaction', { id: responseTransaction.id })
+        return
       } catch (e) {
-        this.updateSearchCount(e);
+        this.updateSearchCount(e)
       }
     },
 
-    updateSearchCount(err) {
+    updateSearchCount (err) {
       if (err !== null) {
-        console.log(err.message || err.data.error);
+        console.log(err.message || err.data.error)
       }
 
       // Increment counter to keep track of whether we found anything
-      this.searchCount += 1;
+      this.searchCount += 1
       if (this.searchCount === 6) {
         // Should match total amount of callbacks
-        this.nothingFound = true;
-        setTimeout(() => (this.nothingFound = false), 1500);
+        this.nothingFound = true
+        setTimeout(() => (this.nothingFound = false), 1500)
       }
     },
 
-    setMobilePlaceholder(showMobile) {
+    setMobilePlaceholder (showMobile) {
       this.placeholder = showMobile
-        ? this.$i18n.t("Search")
-        : this.$i18n.t("Find a block, transaction, address or delegate");
+        ? this.$i18n.t('Search')
+        : this.$i18n.t('Find a block, transaction, address or delegate')
     },
 
-    changePage(name, params) {
-      this.$router.push({ name, params });
-      this.$store.dispatch("ui/setHeaderType", null);
+    changePage (name, params) {
+      this.$router.push({ name, params })
+      this.$store.dispatch('ui/setHeaderType', null)
     },
 
-    findByNameInKnownWallets(name) {
+    findByNameInKnownWallets (name) {
       if (name !== null) {
         for (const address in this.knownWallets) {
           if (this.knownWallets.hasOwnProperty(address)) {
             if (
               name.toLowerCase() === this.knownWallets[address].toLowerCase()
             ) {
-              return address;
+              return address
             }
           }
         }
       }
     }
   }
-};
+}
 </script>
 
 <style>