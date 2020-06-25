a380d2a7fec6c8bfb6db686523458f4224cd290d
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-06-25 17:45:57 +0200

    refactor: remove dead code

--- src/components/tables/DelegateMonitor.vue
+++ src/components/tables/DelegateMonitor.vue
@@ -98,55 +98,6 @@ export default class TableDelegates extends Vue {
     return this.activeTab === "resigned";
   }
 
-  private lastForgingTime(delegate: IDelegate) {
-    return delegate.blocks.last
-      ? // Comment to keepe ts-ignore in check
-        // @ts-ignore
-        this.readableTimestampAgo(delegate.blocks.last.timestamp.unix)
-      : this.$i18n.t("PAGES.DELEGATE_MONITOR.NEVER");
-  }
-
-  private statusTooltip(row: any) {
-    return {
-      trigger: "hover click",
-      content: this.tooltipContent(row),
-      classes: [`tooltip-bg-${this.status(row)}`, "font-sans"],
-    };
-  }
-
-  private tooltipContent(row: any) {
-    // @ts-ignore
-    const status = {
-      0: this.$i18n.t("PAGES.DELEGATE_MONITOR.STATUS.FORGING"),
-      1: this.$i18n.t("PAGES.DELEGATE_MONITOR.STATUS.MISSING"),
-      2: this.$i18n.t("PAGES.DELEGATE_MONITOR.STATUS.NOT_FORGING"),
-      3: this.$i18n.t("PAGES.DELEGATE_MONITOR.STATUS.NEVER_FORGED"),
-      4: this.$i18n.t("PAGES.DELEGATE_MONITOR.STATUS.BECAME_ACTIVE"),
-    }[row.forgingStatus];
-
-    const lastBlock = row.blocks.last;
-
-    return lastBlock
-      ? `[${status}] ${this.$i18n.t("PAGES.DELEGATE_MONITOR.TOOLTIP", {
-          height: lastBlock.height,
-        })} ${
-          // @ts-ignore
-          this.readableTimestamp(lastBlock.timestamp.unix)
-        }`
-      : status;
-  }
-
-  private status(row: any) {
-    // @ts-ignore
-    return {
-      0: "forging",
-      1: "missed-round",
-      2: "not-forging",
-      3: "not-forging", // never-forged
-      4: "became-active",
-    }[row.forgingStatus];
-  }
-
   private sortByLastBlockHeight(x: number, y: number, col: number, rowX: any, rowY: any) {
     const heightX = rowX.blocks.last ? rowX.blocks.last.height : -1;
     const heightY = rowY.blocks.last ? rowY.blocks.last.height : -1;
