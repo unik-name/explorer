c0b84556bc13f532ad5b02f7a7da2f105ddde179
rigwild <me@rigwild.dev>
2020-09-18 19:18:13 +0200

    Link Unikname when wallet is a delegate

--- src/components/wallet/Delegate.vue
+++ src/components/wallet/Delegate.vue
@@ -3,7 +3,11 @@
     <div class="list-row-border-b">
       <div>{{ $t("WALLET.DELEGATE.DELEGATE") }}</div>
       <div v-if="delegate.unikname">
-        <UnikDisplay style="color: inherit" :unikname="delegate.unikname" :type="delegate.unikType" />
+        <LinkUNIK
+          :id="delegate.username"
+          :unikname="delegate.unikname"
+          :type="delegate.unikType"
+        ></LinkUNIK>
       </div>
       <div v-else>{{ delegate.username }}</div>
     </div>
